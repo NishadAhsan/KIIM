ceee63aa35705b294d8bee94aa949d2168b65403
Nishad Ahsan <nishadahsan@gmail.com>
2015-03-03 12:14:32 +0600

    First Commit

---
+++ KIIM/Controllers/KIIMController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KIIM.Controllers
+{
+    public class KIIMController : Controller
+    {
+        //
+        // GET: /KIIM/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+	}
+}
