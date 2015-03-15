e0b02b16ef536e791d41fc609c5144ec63b86099
Nishad Ahsan <nishadahsan@gmail.com>
2015-03-15 13:21:59 +0600

    afterModel

---
+++ KIIM/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using KIIM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KIIM.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Product/Details/5
+        public ActionResult Details()
+        {
+            Product PEntity = new Product { productId = 2654, productName = "Butterfly", productNo = "PNKI/7788", productPrice = 51.33m }; 
+            return View(PEntity);
+        }
+
+        //
+        // GET: /Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ KIIM/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using DataAnnotationsExtensions;
+
+namespace KIIM.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+
+        [Required]
+        [Display(Name = "Name ")]
+        public string productName { get; set; }
+        
+        [Required]
+        [RegularExpression(@"[0-9]{6,10}", ErrorMessage = "Product No. must be between 6-10 digits.")]
+        [Display(Name = "Product No. #")]
+        public string productNo { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price ")]
+        public decimal productPrice { get; set; }
+
+    }
+}
